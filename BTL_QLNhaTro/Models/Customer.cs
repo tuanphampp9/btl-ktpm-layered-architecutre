a04c02159dcfa0c1ea0bab8aa311af9fc1a57a61
tuanphampp9 <tuanphampp9@gmail.com>
2024-12-05 00:11:17 +0700

    ad customer and owner file

---
+++ BTL_QLNhaTro/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTL_QLNhaTro.Models
+{
+    internal class Customer: User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Gender { get; set; }
+    }
+}
---
+++ BTL_QLNhaTro/Models/Owner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTL_QLNhaTro.Models
+{
+    internal class Owner: User
+    {
+        public int Id { get; set; }
+        public string email { get; set; }
+        public string phoneNumber { get; set; }
+        public string fullName { get; set; }
+        public DateTime dob { get; set; }
+        public int gender { get; set; }
+    }
+}
